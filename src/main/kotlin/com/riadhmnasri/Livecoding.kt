ceb795a8022474c5159398e867ff7a9bf4d58b7e
Riadh Mnasri <rmnasri.ext@galerieslafayette.com>
2020-03-12 22:57:33 +0100

    Live coding Kotlin - iteration 2 - start

--- src/main/kotlin/com/riadhmnasri/Livecoding.kt
+++ src/main/kotlin/com/riadhmnasri/Livecoding.kt
@@ -1,5 +1,6 @@
 package com.riadhmnasri
 
+import org.springframework.core.io.ClassPathResource
 import com.riadhmnasri.domain.Book as BookDomain
 
 fun main() {
@@ -30,137 +31,77 @@ fun main() {
      */
 
     // Java vs Kotlin (By example)
+
     // Val vs Vars
-    //val text = "Hello Kotlin"
-    //text = "test" // Val can not reassigned
-    //var text2 = "Hello Kotlin"
 
     // Type Inference (from variable, from interfaces)
-    //val i = 1
-    //println(i.plus(2))
+
 
     // Class and data classes (classes instantiating (no more new))
-    //Referential equality and structural equality
-    //val kotlinBook = Book("ISBNTEST1", "Programming Kotlin")
-    //val kotlinBook2 = Book("ISBNTEST1", "Programming Kotlin")
-    //println(kotlinBook==kotlinBook2)
+    // Referential equality and structural equality
 
-    //Default values (no need to overload)
-    // See Book class constructor
+    // Default values (no need to overload)
+    // Example with Book class constructor
 
     // String templates
-    //val text = "Hello Kotlin"
-    //println("message: $text")
-    /*val paragraph = """
-        Hello
-        Kotlin
-        :-)
-    """.trimIndent()*/
-    //println(paragraph)
 
 
-    //Referential equality and structural equality
-    /* val kotlinBook = BookDomain("ISBNTEST1", "Programming Kotlin")
-     val kotlinBook2 = BookDomain("ISBNTEST1", "Programming Kotlin")
-     println(kotlinBook===kotlinBook2) */
-    // Exception (all are unchecked exceptions)/annotation
-    // All exceptions are unchecked
+    // Referential equality and structural equality
 
-    //Null Safety/Declare Not Nullable types/Elvis operator
+    // Exception handling (all are exceptions unchecked )/annotation
 
+    // Null Safety/Declare Not Nullable types/Elvis operator
+    // Functions in Kotlin
     // Null Safety
-    //val foundBook = BookRepository().findBookByIsbn("ISBNTESTTTT")
-    //println("found book: ${foundBook?.title}")
 
     // Elvis Operator
-    //val result = BookRepository().findBookByIsbn("ISBN") ?: Book("ISBNTEST3", "Domain Driven Design")
-    //println(result)
 
     // Smart casts/explicit casts
-    //print("Text")
 
     // Import renaming
-    //val bookDomain = BookDomain("ISBNTEST1", "Programming Kotlin")
 
     // Ranges/loops
-    /* val rangInts = 1..10 //1 until 10
-    for (i in rangInts){
-        println(i)
-    } */
 
     // When expression/Sealed class
-    /*val result = BookRepository().findBookByIsbnV2("ISBNTEST1")
-    when (result) {
-        is BookResult.BookNotFound -> println("Sorry, Not found!")
-        is BookResult.BookFound -> println("Cool !")
-    }*/
 
     // Inheritance (default final, open, override)
-    //println(Car().name())
-    //Delegation
-    /* val activity = Activity(Tennis())
-    println("${activity.name()}-${activity.cost()}")*/
-    //Functions in Kotlin
+
+    // Delegation
+
+    // Functions in Kotlin
     // Already spoken about
+
     // Named parameters
-    /* val result = BookRepository().findBookByIsbnV2(isbn = "ISBNTEST1")
-    val bookSample = BookDomain(isbn="ISBNTEST1", title = "Programming Kotlin", price = 30.0 )
-    println(bookSample)*/
+
     // Extensions functions
-    //val book = BookDomain("ISBNTEST1", "Programming Kotlin")
-    //println(book.rate(5))
-    //Standard library functions
+
+    // Standard library functions
     // Apply // returns the original instance
-    //val book = BookDomain("ISBNTEST1", "Programming Kotlin").apply { println("rate: ${rate(5)}") }
-    //println(book)
-    // Let // Kind of map to an object
-    //val book = BookDomain("ISBNTEST1", "Programming Kotlin").let { it.rate(5) }
-    //println(book)
+
+    // Let // it's a kind of map to an object
+
     // With
-    //val book = BookDomain("ISBNTEST1", "Programming Kotlin")
-    //val result = with(book){
-    //    rate(5)
-    // }
-    //println(result)
 
     // Run
-    //    val book = BookDomain("ISBNTEST1", "Programming Kotlin")
-    //    val result = book.run {
-    //        rate(5)
-    //    }
-    //    println(result)
+
     // Lazy
-    // val note = lazy { BookDomain("ISBNTEST1", "Programming Kotlin").rate(5) }
-    //println(note.value)
 
     // Use
-    //val text = ClassPathResource("/data/content.txt").inputStream.use { it.bufferedReader().readText() }
-    //println(text)
 
     // Collections (Mutable, Immutable)
     // Immutable collections
-    // val ints = listOf(5, 2, 6, 4, 7, 9, 8) // Immutable
-    // val result = ints.asSequence().filter { it % 2 == 0 }.also { println(it) }.toList()
-    // println(result)
 
     // Mutable collections
-    // val mutableListInts = mutableListOf(5, 2, 6, 4, 7, 9, 8)
-    // mutableListInts.clear()
-    // println(mutableListInts)
+
     // Operator overloading: if we have time
-/*    val kotlinBook = BookDomain("ISBNTEST1", "Programming Kotlin", 30.0)
-    val dddBook = BookDomain("ISBNTEST2", "DDD", 40.0)
-    println(kotlinBook+dddBook)*/
+
     // Mini DSL: if we have time
-    //println("★★★★" shouldBeEqualTo BookDomain("ISBNTEST1", "Programming Kotlin", 30.0).rate(5))
 
 }
 
 
-infix fun <T> T.shouldBeEqualTo(expected: T?): Boolean = this == expected
+infix fun <T> T.shouldBeEqualTo(expected: T?) = this == expected
 
-
-operator fun BookDomain.plus(book: com.riadhmnasri.domain.Book) = copy(isbn = this.isbn + book.isbn, title = this.title + book.title, price = this.price + book.price)
 
 fun BookDomain.rate(note: Int): String = when (note) {
     in 1..5 -> "★".repeat(note)
